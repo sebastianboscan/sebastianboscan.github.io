44a8ed4ba1f2f900761698b9300b16cfef6e59bd
sebastianboscan <sebastianboscan@hotmail.fr>
2024-10-10 13:36:26 -0400

    Tweaked a few things

--- degreeview/script.js
+++ degreeview/script.js
@@ -1,9 +1,5 @@
 // JavaScript function to toggle the menu visibility
 function toggleMenu() {
     var menu = document.getElementById("navMenu");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
+    menu.classList.toggle("show");
 }
