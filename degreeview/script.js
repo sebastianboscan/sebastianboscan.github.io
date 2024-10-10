cd7b8eb74182592600cf89a31d8bbc83b2f8fac8
sebastianboscan <sebastianboscan@hotmail.fr>
2024-10-10 13:31:40 -0400

    added more funcionality to degreeview

---
+++ degreeview/script.js
@@ -0,0 +1,9 @@
+// JavaScript function to toggle the menu visibility
+function toggleMenu() {
+    var menu = document.getElementById("navMenu");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
