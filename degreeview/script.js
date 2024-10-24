f17e6db38e312c542c9ffc1ffe6bf2bc14693516
sebastianboscan <sebastianboscan@hotmail.fr>
2024-10-24 16:39:01 -0400

    Removed the DegreeView folder as I will continue development of the tool privately.

--- degreeview/script.js
+++
@@ -1,9 +0,0 @@
-// JavaScript function to toggle the menu visibility
-function toggleMenu() {
-    var menu = document.getElementById("navMenu");
-    if (menu.style.display === "block") {
-        menu.style.display = "none";
-    } else {
-        menu.style.display = "block";
-    }
-}
